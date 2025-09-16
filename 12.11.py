4abd5ee81cade0830c10311f3ef5517af6468021
KOLIBRI-s2302 <alex.shatalov77@mail.ru>
2025-09-16 06:56:50 +0300

    12.11.py

--- 12.11.py
+++ 12.11.py
@@ -11,7 +11,7 @@ for i in slova:
     bykva_last.append(i[-1])
 for k in range (0,len(bykva_last)):
     j = bykva_last[k].upper()
-    bykva_last.insert(k,j)
+    bykva_last.insert(k, j)
     bykva_last.pop(k + 1)
 if len(bykva_last) != 1:
     for i in range(1,len(slova)):
@@ -38,6 +38,7 @@ if len(bykva_last) != 1:
             print('Победил Петя')
 else:
     print('Вася не назвал слово')
+
 
 
 
