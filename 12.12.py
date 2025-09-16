0ab9d5904c29fb6db6cdb77334c169d22220d488
KOLIBRI-s2302 <alex.shatalov77@mail.ru>
2025-09-16 06:54:47 +0300

    12.12.py

--- 12.12.py
+++ 12.12.py
@@ -1,22 +1,23 @@
 from keyword import kwlist
-allowed_lett=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+allowed_lett = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
               'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
               'u', 'v', 'w', 'x', 'y', 'z','_', 'A',
               'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
               'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
               'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-              'Z','1','2','3','4','5','6','7','8','9','0']
-text=input()
+              'Z', '1', '2', '3', '4', '5', '6',
+              '7', '8', '9', '0']
+text = input()
 if text in kwlist:
     print ('Это слово ключевое')
-    text=''
+    text = ''
 if text[0].isdigit():
     print ('Имя не может начинаться с цифры')
-    text=''
+    text = ''
 for i in text:
     if i not in allowed_lett:
-        print('В имени присутствует недопустимый знак')
+        print ('В имени присутствует недопустимый знак')
         text = ''
         break
-if text!='':
-    print('Это имя подходит')
+if text!= '':
+  print ('Это имя подходит')
