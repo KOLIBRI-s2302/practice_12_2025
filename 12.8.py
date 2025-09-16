b9a65fd51627bf4443cc0417a2bf3c9e720d33e7
KOLIBRI-s2302 <alex.shatalov77@mail.ru>
2025-09-16 07:10:23 +0300

    12.8.py

--- 12.8.py
+++ 12.8.py
@@ -1,23 +1,23 @@
-text=input()
-text=text+' '
-done_text=''
-slova=[]
-while text!='':
-    a=text.find(' ')
+text = input()
+text = text+' '
+done_text = ''
+slova = []
+while text != '':
+    a = text.find(' ')
     slova.append(text[0:a])
-    text=text[a+1:len(text)]
-ctz=[]
+    text = text[a+1:len(text)]
+ctz = []
 for i in slova:
-    ct=0
+    ct = 0
     for j in i:
-        ct+=1
+        ct += 1
     ctz.append(ct)
-ctz.sort(reverse=True)
+ctz.sort(reverse = True)
 print(ctz)
 print(slova)
 for i in ctz:
     for j in slova:
-        if i==len(j):
-            done_text+=j+' '
+        if i == len(j):
+            done_text += j + ' '
             slova.remove(j)
 print(done_text)
