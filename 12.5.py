a1927399373fbd793dffe0f1176a89b8016e31d7
KOLIBRI-s2302 <alex.shatalov77@mail.ru>
2025-09-16 07:04:36 +0300

    12.5.py

--- 12.5.py
+++ 12.5.py
@@ -1,9 +1,9 @@
-text1=input()
-text2=input()
-text3=input()
-bykvi1=set()
-bykvi2=set()
-bykvi3=set()
+text1 = input()
+text2 = input()
+text3 = input()
+bykvi1 = set()
+bykvi2 = set()
+bykvi3 = set()
 for i in text1:
     if i not in bykvi1:
         bykvi1.add(i)
@@ -13,12 +13,12 @@ for i in text2:
 for i in text3:
     if i not in bykvi3:
         bykvi3.add(i)
-bykvi1_2=bykvi1|bykvi2
-bykvi1_3=bykvi1|bykvi3
-bykvi2_3=bykvi2|bykvi3
-un1=[]
-un2=[]
-un3=[]
+bykvi1_2 = bykvi1|bykvi2
+bykvi1_3 = bykvi1|bykvi3
+bykvi2_3 = bykvi2|bykvi3
+un1 = []
+un2 = []
+un3 = []
 for x in bykvi1:
     if x not in bykvi2_3:
         un1.append(x)
