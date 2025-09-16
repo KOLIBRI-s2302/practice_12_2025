f17798146db550b3a145a0a0bbd8d724d8b4f7fc
KOLIBRI-s2302 <alex.shatalov77@mail.ru>
2025-09-16 07:05:35 +0300

    12.6.py

--- 12.6.py
+++ 12.6.py
@@ -1,12 +1,13 @@
-text=input()
-slova=[]
-rev_text=''
-text=text+' '
-while text!='':
+text = input()
+slova = []
+rev_text = ''
+text = text + ' '
+while text != '':
     a=text.find(' ')
     slova.append(text[0:a])
-    text=text[a+1:len(text)]
-slova=slova[::-1]
-for i in range (0,len(slova)):
-    rev_text+=slova[i]+' '
+    text = text[a+1:len(text)]
+slova = slova[::-1]
+for i in range (0, len(slova)):
+    rev_text += slova[i] + ' '
 print(rev_text)
+
