c91ee85489b0687a67e6e789c1c66e4e3d7d56a7
KOLIBRI-s2302 <alex.shatalov77@mail.ru>
2025-09-16 06:47:01 +0300

    12.1.py

--- 12.1.py
+++ 12.1.py
@@ -1,10 +1,11 @@
-text=input()
-prbl=0
-maxim=-1
+text = input()
+prbl = 0
+maxim = -1
 for i in text:
-    if i==' ':
-        prbl+=1
+    if i == ' ':
+        prbl += 1
     else:
-        maxim=max(prbl,maxim)
-        prbl=0
-print(maxim)
+        maxim = max(prbl , maxim)
+        prbl = 0
+print (maxim)
+
